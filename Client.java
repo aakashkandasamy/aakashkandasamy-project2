15f421490fd62635d08932cafa11f653a47110a3
aakashkandasamy <aakashkandasamy@gmail.com>
2024-11-08 14:44:00 -0500

    Setting socket connections

--- Client.java
+++ Client.java
@@ -1,1 +1,26 @@
-// init
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    private Socket socket;
+
+    public Client(String host, int port) throws IOException {
+        this.socket = new Socket(host, port);
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void handshake() {
+        // Not implemented yet
+    }
+
+    public String request(String number) {
+        return null;  // Not implemented yet
+    }
+
+    public void disconnect() throws IOException {
+        socket.close();
+    }
+}
--- Server.java
+++ Server.java
@@ -1,1 +1,35 @@
-// init
+import java.io.*;
+import java.net.*;
+import java.time.*;
+import java.util.*;
+
+public class Server {
+    private ServerSocket serverSocket;
+
+    public Server(int port) throws IOException {
+        this.serverSocket = new ServerSocket(port);
+    }
+
+    public void serve(int clientCount) {
+        for (int i = 0; i < clientCount; i++) {
+            try {
+                Socket clientSocket = serverSocket.accept();
+                // No handshake or request handling implemented yet
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public ArrayList<LocalDateTime> getConnectedTimes() {
+        return new ArrayList<>();  // Empty implementation
+    }
+
+    public void disconnect() {
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
